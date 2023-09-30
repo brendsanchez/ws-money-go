72ae5d1a4725ed503ce519bff33c835de9a3eadf
Brenda Sanchez <brendasanchez9310@gmail.com>
2023-09-30 17:22:06 -0300

    quotations

--- internal/app/scraping/dolarito_scraping.go
+++ internal/app/scraping/dolarito_scraping.go
@@ -9,34 +9,31 @@ import (
 	"github.com/gocolly/colly"
 )
 
-// Define el struct para representar la lista de "quotations"
-type QuotationsList struct {
-	Quotations []Quotation `json:"quotations"`
+type Dolarito struct {
+	Props Props `json:"props"`
+}
+
+type Props struct {
+	PageProps PageProps `json:"pageProps"`
+}
+
+type PageProps struct {
+	RealTimeQuotations RealTimeQuotations `json:"realTimeQuotations"`
+}
+
+type RealTimeQuotations struct {
+	Quotations map[string]Quotation `json:"quotations"`
 }
 
 // Define el struct para representar un elemento de "quotations"
 type Quotation struct {
-	Name      string            `json:"name"`
-	Buy       string            `json:"buy"`
-	Sell      string            `json:"sell"`
-	Timestamp int64             `json:"timestamp"`
-	Variation string            `json:"variation"`
-	Spread    string            `json:"spread"`
-	Volume    map[string]string `json:"volumen"`
-	Extra     ExtraInfo         `json:"extra"`
-}
-
-// Define el struct para representar la información adicional en "extra"
-type ExtraInfo struct {
-	ReferenceBuy1 struct {
-		Name  string  `json:"name"`
-		Price float64 `json:"price"`
-	} `json:"referenceBuy1"`
-
-	ReferenceBuy2 struct {
-		Name  string  `json:"name"`
-		Price float64 `json:"price"`
-	} `json:"referenceBuy2"`
+	Name      string `json:"name"`
+	Buy       string `json:"buy"`
+	Sell      string `json:"sell"`
+	Timestamp int64  `json:"timestamp"`
+	Variation string `json:"variation"`
+	Spread    string `json:"spread"`
+	Volume    string `json:"volumen"`
 }
 
 type dolaritoWS struct {
@@ -50,8 +47,8 @@ func NewDolaritoWS(route string) app.Dollar {
 func (hc *dolaritoWS) GetPrices() (*[]dto.Dollar, error) {
 	c := colly.NewCollector()
 
-	dollarTypes := make([]dto.Dollar, 0, 6)
-	var quotationsList QuotationsList
+	dollarTypes := make([]dto.Dollar, 0, 16)
+	var dolarito Dolarito
 
 	// Visita la URL y analiza la respuesta
 	c.OnHTML("script#__NEXT_DATA__", func(e *colly.HTMLElement) {
@@ -59,12 +56,12 @@ func (hc *dolaritoWS) GetPrices() (*[]dto.Dollar, error) {
 		scriptContent := e.Text
 
 		// Decodifica el contenido JSON directamente en el struct quotationsList
-		if err := json.Unmarshal([]byte(scriptContent), &quotationsList); err != nil {
+		if err := json.Unmarshal([]byte(scriptContent), &dolarito); err != nil {
 			fmt.Println("Error al decodificar JSON:", err)
 		}
 
 		// Ahora puedes acceder a la lista de "quotations" y sus elementos
-		for _, quotation := range quotationsList.Quotations {
+		for _, quotation := range dolarito.Props.PageProps.RealTimeQuotations.Quotations {
 			fmt.Println("Nombre:", quotation.Name)
 			fmt.Println("Cotización de compra:", quotation.Buy)
 			fmt.Println("Cotización de venta:", quotation.Sell)
